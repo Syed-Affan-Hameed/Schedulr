a2b6bc2a78731beb5a754f4b5c961f3e0cf6fb3e
Syed Affan Hameed <syedaffanhameed@gmail.com>
2025-01-26 19:34:53 +0530

    Schedulr setup commit

---
+++ backend/src/routes/schedulr.routes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const schedulrRouter = express.Router();
+
+schedulrRouter.get("/",(req,res)=>{
+    res.send("Welcome to the schedulr route");
+});
---
+++ backend/src/server.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5003;
+//middleware to use json throughout our application
+app.use(express.json());
+// Define allowed origins
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5009", 
+  ];
+  
+  // CORS Middleware
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+  
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        } else {
+          return callback(new Error("Not allowed by CORS")); 
+        }
+      },
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+      credentials: true, 
+      allowedHeaders: ["Content-Type", "Authorization"], 
+    })
+  );
+
+ // app.use("/api/v1/schedulr",schedulrRouter);
+  app.listen(PORT,()=>{
+    console.log("Server is running in the port "+ PORT);
+  });
+
+  app.get("/",(req,res)=>{
+    res.send("Welcome to the server");
+  });
